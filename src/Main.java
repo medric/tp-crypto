fff76105793663d561691076070b42a31d7b9325
medric <medric360@gmail.com>
2016-02-13 14:57:11 +0100

    ajout src

---
+++ src/Main.java
@@ -0,0 +1,187 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class Main {
+	public static void main(String[] args) {
+		// Q.1
+		/*BigInteger[] ns = { new BigInteger("48"), new BigInteger("53") };
+		
+		boolean displayed = true;
+		for (BigInteger n : ns) {
+			for (BigInteger x = BigInteger.ZERO; x.compareTo(n) == -1; x = x
+					.add(BigInteger.ONE)) {
+				displayed = false;
+				for (BigInteger y = BigInteger.ZERO; y.compareTo(n) == -1; y = y
+						.add(BigInteger.ONE)) {
+					if (x.mod(n).compareTo(y.multiply(y).mod(n)) == 0) {
+						if (!displayed) {
+							System.out.println(x + " : ");
+							displayed = true;
+						}
+						System.out.println("\t => " + y);
+					}
+				}
+			}
+			System.out.println("-------------------------------");
+		}*/
+		
+		// Q.3
+		/*BigInteger i = new BigInteger("54");
+		
+		isPrime(i);*/
+		
+		// QAndA
+		try {
+			QAndA();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void isPrime(BigInteger i) {
+		boolean isPrime = true;
+		
+		for(BigInteger x = BigInteger.ZERO; x.compareTo(i) == -1; x.add(BigInteger.ONE)) {
+			System.out.println("current " + x.toString());
+			if(i.mod(x).equals(0)) {
+				isPrime = false;
+			}
+		}
+		
+		if(isPrime) {
+			System.out.println(i + " is prime");
+		} else {
+			System.out.println(i + " is not prime");
+		}
+	}
+	
+	private final static Random rand = new Random();
+
+    private static BigInteger getRandomFermatBase(BigInteger n)
+    {
+        // Rejection method: ask for a random integer but reject it if it isn't
+        // in the acceptable set.
+
+        while (true)
+        {
+            final BigInteger a = new BigInteger (n.bitLength(), rand);
+            // must have 1 <= a < n
+            if (BigInteger.ONE.compareTo(a) <= 0 && a.compareTo(n) < 0)
+            {
+                return a;
+            }
+        }
+    }
+
+    private static boolean checkPrime(BigInteger n, int maxIterations)
+    {
+	    if (n.equals(BigInteger.ONE))
+	        return false;
+	
+	    for (int i = 0; i < maxIterations; i++)
+	    {
+	        BigInteger a = getRandomFermatBase(n);
+	        a = a.modPow(n.subtract(BigInteger.ONE), n);
+	
+	        if (!a.equals(BigInteger.ONE))
+	            return false;
+	    }
+	
+	    return true;
+    }
+    
+    private static BigInteger getRandomPrimeNumber(int length) {
+        while (true)
+        {
+            final BigInteger a = new BigInteger (length, 0, rand);
+            // must have 1 <= a < n
+            if (checkPrime(a, 1000))
+            {
+                return a;
+            }
+        }
+    }
+    
+    // Q.6
+    public static void computePq() {
+    	BigInteger p = getRandomPrimeNumber(512);
+    	BigInteger q = getRandomPrimeNumber(512);
+    	
+    	BigInteger n = p.multiply(q);
+    	BigInteger o = p.subtract(BigInteger.ONE).multiply(p.subtract(BigInteger.ONE)); 
+    }
+    
+    // Q.7
+    public static BigInteger primeWithO(BigInteger o) {
+    	int it = 0;
+    	while(++it < 2000) {
+    		BigInteger e = getRandomPrimeNumber(16);
+        	
+    		if(e.gcd(o).equals(BigInteger.ONE)) {
+    			return e;
+    		}
+    		
+    	}
+    	
+    	return null;
+    }
+    
+    public static void modularInverse(BigInteger o) {
+    	BigInteger e = getRandomPrimeNumber(16);
+    	BigInteger d = e.modInverse(o);
+    }
+    
+    // PROJET 
+    
+    public static void QAndA() throws Exception {
+    	ArrayList<String> answers = new ArrayList<String>();
+    	QAndA qa = new QAndA();
+    	
+    	// List of answers
+    	answers.add("solution");
+    	answers.add("password");
+    	answers.add("validation");
+    	answers.add("secret");
+    	answers.add("admin");
+    	
+    	ArrayList<BigInteger> mToBigInt = (ArrayList<BigInteger>) answers.stream()
+    					.map(m -> Main.textToBigInteger(m))
+    					.collect(Collectors.toList()); 
+    	
+    	// Choix question
+    	Integer i = 0;
+    	for(BigInteger m : mToBigInt) {
+    		System.out.println(m);
+    		qa.cipher(m);
+    	}
+    	
+    	// Decode reponse
+    	ArrayList<BigInteger> ciphers = qa.send(i); // 	enquire
+    	ArrayList<BigInteger> responses = qa.respond(ciphers); // provider
+    	BigInteger clearedAnswer = qa.decryptSecret(responses, i);
+    	System.out.println(bigIntegerToText(clearedAnswer));
+    }
+    
+    /**
+     * convert to big integer
+     * @param m
+     * @return
+     */
+    public static BigInteger textToBigInteger(String m) {
+    	BigInteger bigInt = new BigInteger(m.getBytes());
+    	return bigInt;
+    }
+    
+    /**
+     * convert back to text
+     * @param bigInt
+     * @return
+     */
+    public static String bigIntegerToText(BigInteger bigInt) {
+    	String textBack = new String(bigInt.toByteArray());
+    	return textBack;
+    }
+}
---
+++ src/QAndA.java
@@ -0,0 +1,54 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+
+public class QAndA {
+	public Paillier p;
+	private Integer ceiling;
+	private ArrayList<BigInteger> answers;
+	
+	public QAndA() throws Exception {
+		this.p = new Paillier(100);
+		this.answers = new ArrayList<BigInteger>();
+	}
+
+	public BigInteger encrypt(BigInteger I, BigInteger i) throws Exception {
+		BigInteger M = I.multiply(this.p.encrypt(i.mod(this.p.getN())).mod(this.p.getNsquare())).modPow(this.p.randomZN(), this.p.getNsquare()); 
+		return M;
+	}
+	
+	public BigInteger decrypt(BigInteger S) throws Exception {
+		return this.p.decrypt(S);
+	}
+	
+	public void cipher(BigInteger m) throws Exception {
+		//System.out.println(this.p.encrypt(m));
+		this.answers.add(this.p.encrypt(m));
+		this.ceiling = this.answers.size();
+	}
+	
+	public ArrayList<BigInteger> send(Integer chosenQuestionNumber) throws Exception {
+		ArrayList<BigInteger> ciphers = new ArrayList<BigInteger>();
+		BigInteger I = BigInteger.valueOf(chosenQuestionNumber.intValue());
+		I = this.p.encrypt(I);
+		for(Integer i = 0; i < this.ceiling; i++) {
+			Integer _i = -i;
+			BigInteger _I = BigInteger.valueOf(_i.intValue());
+			ciphers.add(this.encrypt(I, _I));
+		}
+		return ciphers;
+	}
+
+	public ArrayList<BigInteger> respond(ArrayList<BigInteger> ciphers) {
+		ArrayList<BigInteger> secrets = new ArrayList<BigInteger>();
+		for(Integer i = 0; i < this.ceiling; i++) {
+			BigInteger secret = this.answers.get(i).multiply(ciphers.get(i));
+			secrets.add(secret);
+		}
+		return secrets;
+	}
+	
+	public BigInteger decryptSecret(ArrayList<BigInteger> secrets, Integer chosenQuestionNumber) throws Exception {
+		return this.p.decrypt(secrets.get(chosenQuestionNumber).mod(this.p.getNsquare()));
+	}
+}
